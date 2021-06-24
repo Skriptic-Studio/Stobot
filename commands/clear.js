ba83ee8a077dba3b7b9896072e6c07360f8d3afe
Marcos Menezes Nunes <marcosnunes12340987@gmail.com>
2021-06-24 22:15:19 +0000

    Tidy help command and clear command (collector)

--- commands/clear.js
+++ commands/clear.js
@@ -14,15 +14,16 @@ let thumbnails = require('../config/thumbnail.json')
 if(!args) {
 	message.lineReply(embed.setTitle("**Are you sure?**").setDescription("This command without arguments deletes all your storage.").setFooter("Answer with 'yes'|'y', or 'no'|'n'."))
 	
-	let filter = msg => msg.author.id == message.author.id && (msg.content == 'y'||msg.content == 'yes'||msg.content == 'no'||msg.content == 'no');
+	let filter = msg => msg.author.id == message.author.id && (msg.content == 'y'||msg.content == 'yes'||msg.content == 'n'||msg.content == 'no');
 	const collector = message.channel.createMessageCollector(filter, { time: 15000 });
 	collector.on("collect", async msg => {
 		if(msg.content == 'y'||msg.content == 'yes'){
 			await userStorage.delete(`user - ${msg.author.id}`);
-			return message.lineReply("All your storage was succesfully cleared");
+			message.lineReply("All your storage was succesfully cleared");
 		} else {
 			return message.lineReply("Proccess Cancelled")
 		}
+		collector.stop()
 	})
 }
 if(args=="all") {
--- commands/help.js
+++ commands/help.js
@@ -11,18 +11,20 @@ const embed = new Discord.MessageEmbed()
 
 message.lineReply(embed.setTitle('***Help***').setDescription('Here you can see all commands of a given type\n\n**Press the type of command that you want:**\n1. Store commands\n2. Get commands\n3. Clear commands').setFooter('Answer 4 to cancel'))
 
-let filter = msg => msg.author.id == message.author.id && (msg.content == '1'||msg.content == '2'||msg.content == '3');
+let filter = msg => msg.author.id == message.author.id;
 const collector = message.channel.createMessageCollector(filter, { time: 15000 });
 collector.on("collect", async msg => {
 	if(msg.content == '1'){
-		return message.lineReply(embed.setTitle('***Get Commands***').setDescription('**s.get**\nGets user information.\n\n**s.getserver**\nGets server information.\n\nUse a information name as argument and get this specific information.'));
+		message.lineReply(embed.setTitle('***Store Commands***').setDescription("**s.store**\nStores user information.\n\n**s.storeserver**\nStores server information.\n\nIts needed to give all arguments separated by '|'").setFooter('You can see more detais using s.help <command>'));
+		
 	} else if(msg.content == '2') {
-		return message.lineReply(embed.setTitle('***Store Commands***').setDescription("**s.store <name>|<description>|<detail>**\nStores user information.\n\n**s.storeserver <name>|<description>|<detail>**\nStores server information.\n\nIts needed to give all arguments separated by '|'"));
+		message.lineReply(embed.setTitle('***Get Commands***').setDescription('**s.get**\nGets user information.\n\n**s.getserver**\nGets server information.\n\nUse an information name as argument and get this specific information.').setFooter('You can see more detais using s.help <command>'));
 	} else if(msg.content == '3') {
-		return message.lineReply(embed.setTitle('***Clear Commands***').setDescription("**s.clear**\n Clears all user information.\n\n**s.clear server**\n Clears all server information."));
+		message.lineReply(embed.setTitle('***Clear Commands***').setDescription("**s.clear**\n Clears all user information.\n\n**s.clear server**\n Clears all server information.").setFooter('You can see more detais using s.help <command>'));
 	} else if(msg.content == '4') {
-		return message.lineReply('Canceled');
+		message.lineReply('Canceled');
 	}
+	collector.stop()
 })
 
 	}
